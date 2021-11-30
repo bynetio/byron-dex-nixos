c89ba3961abaf6a186aef1eaf18078ce505890af
Tomasz Hołubowicz <tomasz.holubowicz@binarapps.com>
2021-11-30 14:39:10 +0100

    Add devShell

--- flake.nix
+++ flake.nix
@@ -3,35 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     cardano-node.url = "github:input-output-hk/cardano-node";
   };
 
-  outputs =
-    { nixpkgs
-    , cardano-node
-    , ...
-    }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config = { allowUnfree = true; };
-        overlays = [
-          (import ./packages)
-        ];
-      };
-    in
-    {
-      # devShell.${system} = import ./shell.nix { inherit pkgs; };
-      nixosConfigurations = {
-        ec2-backend-byron-network = nixpkgs.lib.nixosSystem {
-          inherit pkgs system;
-          modules = [
-            ./hosts             # main configuration file for hosts
-            ./roles/common      # common configuration for any instance
-            ./users/dex         # `dex` user setup
+  outputs = { self, nixpkgs, flake-utils, cardano-node, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { allowUnfree = true; };
+          overlays = [
+            (import ./packages)
           ];
         };
-      };
-    };
+      in
+      {
+        nixosConfigurations = {
+          ec2-backend-byron-network = nixpkgs.lib.nixosSystem {
+            inherit pkgs system;
+            modules = [
+              ./hosts # main configuration file for hosts
+              ./roles/common # common configuration for any instance
+              ./users/dex # `dex` user setup
+            ];
+          };
+        };
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ rnix-lsp ];
+        };
+      });
 }
