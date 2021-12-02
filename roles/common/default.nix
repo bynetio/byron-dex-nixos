cc80c1f54e17ecd2412459429243bd7f23945364
Tomasz Hołubowicz <tomasz.holubowicz@binarapps.com>
2021-12-02 09:31:37 +0100

    Minor formatting

--- roles/common/default.nix
+++ roles/common/default.nix
@@ -3,15 +3,12 @@
 {
   # common settings
   networking.firewall.allowedTCPPorts = [ 8080 ];
-  i18n.defaultLocale           = "pl_PL.UTF-8";
-  time.timeZone                = "Europe/Warsaw";
+  i18n.defaultLocale = "pl_PL.UTF-8";
+  time.timeZone = "Europe/Warsaw";
   virtualisation.docker.enable = true;
 
   # IOHK binary cache
-  nix.binaryCaches = [
-    "https://hydra.iohk.io"
-    "https://iohk.cachix.org"
-  ];
+  nix.binaryCaches = [ "https://hydra.iohk.io" "https://iohk.cachix.org" ];
   nix.binaryCachePublicKeys = [
     "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
     "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
@@ -33,11 +30,10 @@
     git
     htop
     tmux
+    ripgrep
     vim
   ];
 
   # vim as a default editor
-  environment.variables = {
-    EDITOR = "vim";
-  };
+  environment.variables = { EDITOR = "vim"; };
 }
