cb6db51f5b1740ea33a829691188ad535b8ccb05
Tomasz Hołubowicz <tomasz.holubowicz@binarapps.com>
2021-12-02 09:26:53 +0100

    Add iohk-nix to flake’s inputs

--- flake.nix
+++ flake.nix
@@ -3,10 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    iohkNix.url = "github:input-output-hk/iohk-nix";
     cardano-node.url = "github:input-output-hk/cardano-node";
   };
 
-  outputs = { self, nixpkgs, cardano-node, ... }:
+  outputs = { self, nixpkgs, iohkNix, cardano-node, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -14,17 +15,20 @@
         config = { allowUnfree = true; };
         overlays = [
           cardano-node.overlay
-          (import ./packages)
-        ];
+          iohkNix.overlays.haskell-nix-extra
+          iohkNix.overlays.crypto
+          iohkNix.overlays.cardano-lib
+          iohkNix.overlays.utils
+          (import ./packages) ];
       };
-    in
-    {
+    in {
       nixosConfigurations = {
         ec2-backend-byron-network = nixpkgs.lib.nixosSystem {
           inherit pkgs system;
           modules = [
             ./hosts # main configuration file for hosts
             ./roles/common # common configuration for any instance
+            ./roles/services # services services configuration
             ./users/dex # `dex` user setup
           ];
         };
